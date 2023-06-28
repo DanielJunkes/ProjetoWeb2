738d9cec2b432df1652833b94f4358482d265d7f
Felipe705 <delavedova44@gmail.com>
2023-06-28 19:21:27 -0300

    aa

--- src/pages/ListarTestes.js
+++ src/pages/ListarTestes.js
@@ -2,12 +2,33 @@ import React from "react";
 import Testes from '../components/testes/index';
 
 
-function ListarTestes() {
-    return (
-        <>
-            <Testes nome="aa"/>
-        </>
-    )
+class ListarTestes extends React.Component() {
+    constructor(props) {
+        super(props)
+        this.API_URL = "http://localhost:8080/data/herois"
+        this.state = {
+            "herois": [],
+            "selecionado": null
+        }
+    }
+
+    
+    getTestes = () => {
+        const requisicao = fetch(this.API_URL)
+         .then((resposta) => {
+            this.setState({
+                "herois": resposta.data
+            })
+        })  
+    }
+
+    render() {
+        return (
+            <>
+                <Testes nome="aa"/>
+            </>
+        )
+    }
 }
 
 export default ListarTestes
