4ce747705699f7504dbd0775bb1aff6cbda1d380
Gilmar <geoh222@unesc.net>
2023-06-30 17:58:10 -0300

    Emagrecimento dos botões e logo nova 😉

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.css"
 
 import logo from "../../img/logo.png"
+//Logo2 = Logo Escuro
+import logo_2 from "../../img/logo2.png"
 
 const Header = () => {
     return (
         <>
             <header className="header">
-                <img className="logo" src={logo} alt="Cérebro"/>
+                <img className="logo" src={logo_2} alt="Cérebro"/>
                 <h1 className="titulo">Teste de Memória</h1>
             </header>
         </>
