b10a72912739d1d929e44afd5b3cac9ef500cd2d
DanielJunkes <danielmafioleti2@gmail.com>
2023-07-12 10:00:43 -0300

    edicao deInputFuncionaPorem n SalvaPerguntaEditada

--- src/components/perguntas/index.js
+++ src/components/perguntas/index.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect} from "react";
 import {Contexto} from '../../routerProvider'
 import "./style.css"
+import InputPergunta from "./inputPergunta";
 
 import {Link} from 'react-router-dom';
 
@@ -59,7 +60,7 @@ const EditarTeste = () =>{
         for(let i=0; i < qntPerguntas+qntPerguntasSalvas; i++) {
             if(i>qntPerguntasSalvas-1) {
                 const perguntas = document.querySelector(`div#pergunta${i}`).childNodes[0];
-
+                
                 const novaPergunta = {
                     titulo: perguntas.childNodes[1].value, // Titulo
                     opcaoA: perguntas.childNodes[3].value, // Opção 1
@@ -161,36 +162,12 @@ const EditarTeste = () =>{
                 </div>
                 <section id="sec">
                 {perguntas.map((pergunta, index)=>(
-                                <div id={`pergunta${index}`} key={index}>
-                                    <art id="art">
-                                        <h2 id="nome">Titulo</h2>
-                                        <input id="nomeI" type="text" value={pergunta.titulo}/>    
-                                        <label for="op">Opção A: </label> 
-                                        <input type="text" id="op1" className="op" value={pergunta.opcaoA}/>
-                                        <label for="op">Opção B: </label> 
-                                        <input type="text" id="op2" className="op" value={pergunta.opcaoB}/>
-                                        <label for="op">Opção C: </label> 
-                                        <input type="text" id="op3" className="op" value={pergunta.opcaoC}/>
-                                        <label for="op"> D: </label> 
-                                        <input type="text" id="op4" className="op" value={pergunta.opcaoD}/>
-                                        <label for="op">Opção E: </label>     
-                                        <input type="text" id="op5" className="op" value={pergunta.opcaoE} />
-                                        <div className="alternativacorreta">
-                                            <label for="alternativacorreta: ">Alternativa correta:</label>
-                                            <select name="cars" id="alternativacorreta" select="A">
-                                                <option selected>{pergunta.resposta}</option>
-                                                <option value="A">A</option>
-                                                <option value="B">B</option>
-                                                <option value="C">C</option>
-                                                <option value="D">D</option>
-                                                <option value="E">E</option>
-                                            </select>
-                                        </div>          
-                                    </art> 
-                                </div>
-                            ))}
+                    <div id={`pergunta${index}`} key={index}>
+                        <InputPergunta pergunta={pergunta}/>
+                    </div>
+                    ))}
                 </section>
-                    <input class="btn" type="button" value="Add Pergunta" onClick={() => addPergunta()}/>
+                    <input class="btn" type="button" value="Add Pergunta" onClick={() => {addPergunta()}}/>
                 <div class="containerBtn">
                     <Link to="/addteste">
                        <input id="salvar" class="btn" type="button" value="Salvar" onClick={() => {btnSalvar()}}/> 
--- src/components/perguntas/inputPergunta/index.js
+++ src/components/perguntas/inputPergunta/index.js
@@ -1,88 +1,46 @@
-import React, { useContext, useState, useEffect, useSyncExternalStore} from "react";
-import {Contexto} from "../../../routerProvider"
+import React, { useContext, useState, useEffect} from "react";
 
-import {Link} from 'react-router-dom';
+const InputPergunta = (pergunta) => {
 
-const InputPergunta = () => {
+    const perguntaA = pergunta.pergunta
 
-    const {teste} = useContext(Contexto)
-    const {usuario} = useContext(Contexto)
-    
+    const [titulo, setTitulo] = useState("")
+    const [opcaoA, setOpcaoA] = useState("")
+    const [opcaoB, setOpcaoB] = useState("")
+    const [opcaoC, setOpcaoC] = useState("")
+    const [opcaoD, setOpcaoD] = useState("")
+    const [opcaoE, setOpcaoE] = useState("")
+    const [resposta, setResposta] = useState("")
 
-    const addPergunta = async () =>{
-        let listaPerguntas = []
-
-        const nome = document.getElementById("nomeI").value
-        const opcaoA = document.getElementById("op1").value
-        const opcaoB = document.getElementById("op2").value
-        const opcaoC = document.getElementById("op3").value
-        const opcaoD = document.getElementById("op4").value
-        const opcaoE = document.getElementById("op5").value
-        const opcaoCerta = document.getElementById("alternativacorreta").value
-
-        const novaPergunta = {
-            titulo: nome,
-            opcaoA: opcaoA,
-            opcaoB: opcaoB,
-            opcaoC: opcaoC,
-            opcaoD: opcaoD,
-            opcaoE: opcaoE,
-            resposta: opcaoCerta
-        }
-
-        try {
-            await fetch('http://localhost:8080/testes/'+teste, {
-                method: "GET",
-                headers: {'Content-type': 'application/json'},
-            })
-            .then(response => {
-                return response.json()
-            })
-            .then(async data => {
-                listaPerguntas = data.perguntas
-            })
-        } catch(e){
-            console.log(e)
-        }
-
-        listaPerguntas.push(novaPergunta)
-        
-        const dados = {
-                id: teste,
-                perguntas: listaPerguntas
-        }
-
-        const dadosJ = JSON.stringify(dados)
-
-        try {
-            await fetch('http://localhost:8080/testes/update', {
-                method: "PUT",
-                headers: {'Content-type': 'application/json'},
-                body: dadosJ
-            })
-        } catch(e){
-            console.log(e)
-        }
-    }
+    useEffect(()=>{
+       setTitulo(perguntaA.titulo)
+       setOpcaoA(perguntaA.opcaoA)
+       setOpcaoB(perguntaA.opcaoB)
+       setOpcaoC(perguntaA.opcaoC)
+       setOpcaoD(perguntaA.opcaoD)
+       setOpcaoE(perguntaA.opcaoE)
+       setResposta(perguntaA.resposta)
+    }, [])
 
     return(
         <>
             <art id="art">
                 <h2 id="nome">Titulo da Pergunta: </h2>
-                <input id="nomeI" type="text"  />    
+                <input id="nomeI" type="text" value={titulo} onChange={(e)=>{setTitulo(e.target.value)}}/>    
                 <label for="op">Opção A: </label> 
-                <input type="text" id="op1" className="op" />
+                <input type="text" id="op1" className="op" value={opcaoA} onChange={(e)=>{setOpcaoA(e.target.value)}}/>
                 <label for="op">Opção B: </label> 
-                <input type="text" id="op2" className="op" />
+                <input type="text" id="op2" className="op" value={opcaoB} onChange={(e)=>{setOpcaoB(e.target.value)}}/>
                 <label for="op">Opção C: </label> 
-                <input type="text" id="op3" className="op" />
+                <input type="text" id="op3" className="op" value={opcaoC} onChange={(e)=>{setOpcaoC(e.target.value)}}/>
                 <label for="op"> D: </label> 
-                <input type="text" id="op4" className="op" />
+                <input type="text" id="op4" className="op" value={opcaoD} onChange={(e)=>{setOpcaoD(e.target.value)}}/>
                 <label for="op">Opção E: </label>     
-                <input type="text" id="op5" className="op" />
+                <input type="text" id="op5" className="op" value={opcaoE} onChange={(e)=>{setOpcaoE(e.target.value)}}/>
                 <div className="alternativacorreta">
                     <label for="alternativacorreta: ">Alternativa correta:</label>
-                    <select name="cars" id="alternativacorreta">
+                    <select name="cars" id="alternativacorreta" select="A" onChange={(e)=>{setResposta(e.target.value)}}>
+                        <option selected>{resposta}</option>
                         <option value="A">A</option>
                         <option value="B">B</option>
                         <option value="C">C</option>
@@ -90,11 +48,6 @@ const InputPergunta = () => {
                         <option value="E">E</option>
                     </select>
                 </div>
-
-                <Link to="/editarteste">
-                    <input type="button" value="Adicionar Pergunta" onClick={() => addPergunta()}/>
-                </Link>
-                    
             </art> 
         </>
     )
